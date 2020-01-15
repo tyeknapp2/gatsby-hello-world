728ec86669c406c888b83dfb5f41bcc69f150179
Tye Knappenberger <tyeknapp.pro@gmail.com>
2020-01-15 14:25:11 -0500

    edit to the contact js file

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,5 +6,14 @@ export default () => (
     <Link to="/">Home</Link>
     <Header headerText="Contact" />
     <p>Send us a message!</p>
+    <blockquote cite="https://www.huxley.net/bnw/four.html">
+      <p>
+        Words can be like X-rays, if you use them properly—they’ll go through
+        anything. You read and you’re pierced.
+      </p>
+      <footer>
+        —Aldous Huxley, <cite>Brave New World</cite>
+      </footer>
+    </blockquote>
   </div>
 )
